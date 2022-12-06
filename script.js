d9d9a65111a2c63285db0a7d6dde144607a191a7
Anju Dhungana <anjudhungana28@gmail.com>
2022-12-06 15:55:32 -0500

    create songlist looping from songs

---
+++ script.js
@@ -0,0 +1,23 @@
+const songs = [
+                "bensound-glitchfidelity.mp3",
+                "bensound-mellowbreeze.mp3",
+                "bensound-mila.mp3",
+                "bensound-slider.mp3",
+                "bensound-thecalling.mp3",
+                "bensound-theday.mp3",   
+];
+
+function createSongList(){
+   const list = document.createElement("ol");
+   for(let i = 0; i < songs.length; i++) {
+    const item = document.createElement("li");
+item.appendChild(document.createTextNode(songs[i]));
+list.appendChild(item);
+   }
+   return list
+}
+
+const songList = document.getElementById('songList')
+ songList.appendChild(createSongList());
+
+ 
