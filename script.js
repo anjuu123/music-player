cfd9c1f3ecf564c8ebfc33f7c7d8a22f784325f8
Anju Dhungana <anjudhungana28@gmail.com>
2022-12-07 08:38:35 -0500

    adjusting volume level

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
 const songs = [
-                "bensound-glitchfidelity.mp3",
-                "bensound-mellowbreeze.mp3",
-                "bensound-mila.mp3",
-                "bensound-slider.mp3",
-                "bensound-thecalling.mp3",
-                "bensound-theday.mp3",   
+  "bensound-glitchfidelity.mp3",
+  "bensound-mellowbreeze.mp3",
+  "bensound-mila.mp3",
+  "bensound-slider.mp3",
+  "bensound-thecalling.mp3",
+  "bensound-theday.mp3",
 ];
 
-function createSongList(){
-   const list = document.createElement("ol");
-   for(let i = 0; i < songs.length; i++) {
+const player = document.getElementById("player");
+//looping through length of song 
+//creating a song list
+
+function createSongList() {
+  const list = document.createElement("ol");
+  for (let i = 0; i < songs.length; i++) {
     const item = document.createElement("li");
-item.appendChild(document.createTextNode(songs[i]));
-list.appendChild(item);
-   }
-   return list
+    item.appendChild(document.createTextNode(songs[i]));
+    list.appendChild(item);
+  }
+  return list;
 }
 
-const songList = document.getElementById('songList')
- songList.appendChild(createSongList());
+const songList = document.getElementById("songList");
+songList.appendChild(createSongList());
 
- songList.onclick = function(e){
-    const source = document.getElementById('source')
-    source.src = "songs/" + e.target.innerText;
+songList.onclick = function (e) {
+  const source = document.getElementById("source");
+  source.src = "songs/" + e.target.innerText;
+  document.querySelector(
+    "#currentsSong"
+  ).innerText = `Now Playing: ${e.target.innerText}`;
+ 
+  player.load();
+  player.play();
+};
 
-    document.querySelector('#currentSong').innerText = `Now Playing: ${e.target.innerText}`
 
-    const player = document.getElementById('player')
-    player.load();
+function playAudio() {
+   if(player.readyState) {
     player.play();
- }
+   }
+  }
 
+function pauseAudio() {
+  player.pause();
+}
+
+const slider = document.getElementById("volumeSlider");
+slider.oninput = function(e) {
+  const volume = e.target.value;
+  player.volume = volume;
+
+}
